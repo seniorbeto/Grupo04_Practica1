f124e1e63b887ce5ac8348484520090ddbf2c4fd
Raúl Aguilar Arroyo <raulagarr@gmail.com>
2024-04-05 10:10:29 +0200

    busqueda de un svr

--- busquedas/bayes_opt.py
+++ busquedas/bayes_opt.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
 from sklearn import metrics
 import optuna
 import numpy as np
@@ -17,20 +17,22 @@ print(wind_ava.head())
 # Dividimos los datos en entrenamiento y test
 X = wind_ava.drop(columns='energy')
 y = wind_ava['energy']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=SEED)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=SEED)
 
 
 def objective(trial):
+    # parametros para un svr
     params = {
-        'n_estimators': trial.suggest_int('n_estimators', 200, 350),
-        'max_depth': trial.suggest_int('max_depth', 20, 40),
-        'min_samples_split': trial.suggest_int('min_samples_split', 2, 7),
-        'min_samples_leaf': trial.suggest_int('min_samples_leaf', 1, 5),
-        'bootstrap':  trial.suggest_categorical('bootstrap', [True])
+        'C': trial.suggest_loguniform('C', 1e-2, 1e2),
+        'epsilon': trial.suggest_loguniform('epsilon', 1e-2, 1e2),
+        'kernel': trial.suggest_categorical('kernel', ['linear', 'poly', 'rbf', 'sigmoid']),
+        'degree': trial.suggest_int('degree', 1, 5),
+        'gamma': trial.suggest_categorical('gamma', ['scale', 'auto']),
+        'coef0': trial.suggest_loguniform('coef0', 1e-2, 1e2)
     }
     # print(f"Trial {trial.number} - Iniciando entrenamiento con hiperparámetros: {params}")
     
-    model = RandomForestRegressor(**params)
+    model = SVR(**params)
     rmse_scores = []
     for _ in range(10):  # 10-fold cross-validation
         X_fold_train, X_fold_val, y_fold_train, y_fold_val = train_test_split(X_train, y_train, test_size=0.1, random_state=SEED)
@@ -62,7 +64,7 @@ df = study.trials_dataframe()
 df.to_csv('optuna_results.csv', index=False)
 
 # Entrenar el mejor modelo con los mejores hiperparámetros
-best_model = RandomForestRegressor(**best_params)
+best_model = SVR(**best_params)
 best_model.fit(X_train, y_train)
 
 # Predecir los valores de test
@@ -72,23 +74,25 @@ y_test_pred = best_model.predict(X_test)
 rmse_rf_cv = metrics.root_mean_squared_error(y_test, y_test_pred)
 r2_rf_cv = best_model.score(X_test, y_test)
 
-print(f"RMSE: {rmse_rf_cv}")
-print(f"R2: {r2_rf_cv}")
+print(f"SVR RMSE: {rmse_rf_cv}")
+print(f"SVR R2: {r2_rf_cv}")
 
 # exportar el mejor modelo
 try:
     import pickle
-    with open(f'bayes_rf_{rmse_rf_cv}.pkl', 'wb') as f:
+    with open(f'bayes_svr_{rmse_rf_cv}.pkl', 'wb') as f:
         pickle.dump(best_model, f)
 except:
     try:
         import joblib
-        joblib.dump(best_model, f'bayes_rf_{rmse_rf_cv}.pkl')
+        joblib.dump(best_model, f'bayes_svr_{rmse_rf_cv}.pkl')
     except:
         pass
     
-print(f"Parametros del mejor modelo: {best_params}")
+print(f"Parametros del mejor model (svr): {best_params}")
     
     
-# parametros obtenidos por la busqueda
-# {'n_estimators': 239, 'max_depth': 25, 'min_samples_split': 2, 'min_samples_leaf': 1, 'bootstrap': True}
+# mejor random forest:
+# {'n_estimators': 333, 'max_depth': 34, 'min_samples_split': 2, 'min_samples_leaf': 2, 'bootstrap': True}
+
+# mejor svr:
